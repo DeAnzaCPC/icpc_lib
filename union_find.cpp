638d75781f0a7cc57458aba19c9bd8c9527f7cb6
Sunho Kim <ksunhokim123@gmail.com>
2023-04-14 19:53:49 -0700

    update

--- union_find.cpp
+++ union_find.cpp
@@ -6,26 +6,15 @@ struct union_find {
   union_find(int n) : n(n), p(n), sz(n, 1) {
     iota(begin(p),end(p), 0);
   }
-  
-  // return the representitive node of connected component
-  int find_p(int x) {
+  int leader(int x) {
     if (p[x] == x)
       return x;
-    // path compression: cache the parent finding
-    return p[x] = find_p(p[x]);
+    return p[x] = leader(p[x]);
   }
-
-  // join the components that x and y belong to
-  bool unite(int x, int y) {
-    int u = find_p(x);
-    int v = find_p(y);
-    if (u == v) return false;
-    // size heuristic: connect smaller tree to larger tree
-    if (sz[u] > sz[v]) {
-      swap(u,v);
-    }
-    p[v] = u;
-    sz[u] += sz[v];
-    return true;
+  void unite(int x, int y) {
+    int l = leader(x), s = leader(y);
+    if (l == s) return;
+    if (sz[s] > sz[l]) swap(s,l);
+    p[s] = l, sz[l] += sz[s];
   }
 };
