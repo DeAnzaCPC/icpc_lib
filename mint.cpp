e3f777321e50900d47cb4ad8663ac29359d591a0
Sunho Kim <ksunhokim123@gmail.com>
2023-05-13 03:03:34 -0700

    Update

--- mint.cpp
+++ mint.cpp
@@ -2,16 +2,25 @@
 // does arithmetics (mod P) automatically
 const int MOD = 1000000007; // 998244353
 struct mint {
-  int v;
-  mint() : v(0) {}
-  mint(ll v) : v(v % MOD) { v += (v < 0) * MOD; }
+  int x;
+  mint(int x = 0) : x(norm(x)) { }
+  mint(ll x) : x(norm(x%MOD)) { }
+  int norm(int x) const { if (x < 0) { x += MOD; } if (x >= MOD) { x -= MOD; } return x; }
+  mint operator+(mint o) const { return norm(x + o.x); }
+  mint operator-(mint o) const { return norm(x - o.x); }
+  mint operator*(mint o) const { return ll(x) * o.x; }
+  mint &operator+=(mint o) { return *this = *this + o; }
+  mint &operator-=(mint o) { return *this = *this - o; }
+  mint &operator*=(mint o) { return *this = *this * o; }
+  mint inv() const { return pow(MOD - 2); }
+  mint pow(ll x) const {
+    mint a = *this;
+    mint b = 1;
+    for (; x; x >>= 1) {
+      if (x & 1)
+        b *= a;
+      a *= a;
+    }
+    return b;
+  }
 };
-mint& operator+=(mint& a, mint b) { if ((a.v += b.v) >= MOD) a.v -= MOD; return a; }
-mint& operator-=(mint& a, mint b) { if ((a.v -= b.v) < 0) a.v += MOD; return a; }
-mint operator*(mint a, mint b) { return mint((ll) a.v * b.v); }
-mint operator+(mint a, mint b) { return a += b; }
-mint operator-(mint a, mint b) { return a -= b; }
-mint& operator*=(mint& a, mint b) { return a = a * b; }
-mint pow(mint a, ll p) { return p == 0 ? 1 : pow(a * a, p / 2) * (p & 1 ? a : 1); }
-mint inv(mint a) { return pow(a, MOD - 2); }
-mint operator/(mint a, mint b) { return a * inv(b); }
